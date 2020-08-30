ba0d841466f8ae2a0eb5063f245738a1475197c2
migvasc <miguel.vasconcelos@usp.br>
2020-08-30 18:27:34 -0300

    primeiro teste red

---
+++ spec/requests/paginas_estaticas_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+require 'spec_helper'
+ 
+describe "Página Inicial" do
+  it "deveria ter o conteúdo 'Bem vindo'" do
+    visit '/'
+    expect(page).to have_content('Bem vindo')
+  end
+end
